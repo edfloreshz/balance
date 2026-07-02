f67615a73f16fd660b7ff4f21ad4b28ec345b759
Eduardo Flores <edfloreshz@gmail.com>
2026-07-01 19:08:10 -0700

    fix: iPadOS and iOS compilation

--- Balance/Balance/Features/Sidebar/SidebarView.swift
+++ Balance/Balance/Features/Sidebar/SidebarView.swift
@@ -14,9 +14,19 @@ enum SidebarSelection: Hashable {
 
 struct SidebarView: View {
 	@Binding var selection: SidebarSelection
+	
+	private var sidebarSelection: Binding<SidebarSelection?> {
+		Binding(
+			get: { selection },
+			set: { newValue in
+				guard let newValue else { return }
+				selection = newValue
+			}
+		)
+	}
 
 	var body: some View {
-		List(selection: $selection) {
+		List(selection: sidebarSelection) {
 			Section {
 				NavigationLink(value: SidebarSelection.dashboard) {
 					Label("Dashboard", systemImage: "chart.xyaxis.line")
